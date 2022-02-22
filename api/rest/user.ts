b30891b6cb85168d27f82efc1ee8cb27de06a10d
hasegawa <hasegawa@abc.com>
2022-02-22 13:29:13 +0900

    ユーザ削除追加（仮）

--- api/rest/user.ts
+++ api/rest/user.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosResponse } from 'axios'
 
-const BASE_URL = 'https://wf-rest.herokuapp.com'
+//const BASE_URL = 'https://wf-rest.herokuapp.com'
+const BASE_URL = 'http://localhost:8080'
 
 export async function getUserList() {
   const res = await axios.get(`${BASE_URL}/users`)
--- pages/userlist.vue
+++ pages/userlist.vue
@@ -17,10 +17,27 @@
         :headers="headers"
         :items="users"
         :search="search"
-        :loading="this.users.length === 0"
+        :loading="isLoading"
         loading-text="Loading..."
-      ></v-data-table>
+      >
+        <template v-slot:[`item.delete`]="{ item }">
+          <v-icon @click="deleteItem(item)"> mdi-delete </v-icon>
+        </template>
+      </v-data-table>
     </v-card>
+    <v-dialog v-model="dialogDelete" max-width="500px">
+      <v-card>
+        <v-card-title class="text-h5">削除してよろしいですか?</v-card-title>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+          <v-btn color="blue darken-1" text @click="deleteItemConfirm"
+            >OK</v-btn
+          >
+          <v-spacer></v-spacer>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </section>
 </template>
 <script lang="ts">
@@ -31,15 +48,22 @@ export default Vue.extend({
     return {
       search: '',
       headers: [
-        { text: 'name', value: 'name' },
-        { text: 'userId', value: 'userId' },
-        { text: 'status', value: 'status' },
+        { text: 'ユーザID', value: 'userId' },
+        { text: '氏名', value: 'name' },
+        { text: '生年月日', value: 'bornDate' },
+        { text: '郵便番号', value: 'postNumber' },
+        { text: '住所', value: 'address' },
+        { text: '有効/無効', value: 'activeFlag' },
+        { text: '', value: 'delete', sortable: false },
       ],
       users: [] as Array<any>,
       isLoading: false,
+      deletedIndex: -1,
+      dialogDelete: false,
     }
   },
   created() {
+    console.log('created')
     this.getUsers()
   },
   methods: {
@@ -48,8 +72,42 @@ export default Vue.extend({
       const users = await getUserList()
       for (const key in users) {
         const u: any = users[key]
-        this.users.push(u)
+        this.users.push(this.fomatData(u))
       }
+      this.isLoading = false
+    },
+    fomatData(user: any) {
+      user.bornDate = user.bornDate.replace('-', '年').replace('-', '月') + '日'
+      user.postNumber =
+        user.postNumber.slice(0, 3) + '-' + user.postNumber.slice(3)
+      user.address =
+        user.address1 +
+        ' ' +
+        user.address2 +
+        ' ' +
+        user.address3 +
+        ' ' +
+        user.address4
+      user.activeFlag = user.activeFlag === 'true' ? '有効' : '無効'
+      user.delete = 'del'
+      return user
+    },
+    deleteItemConfirm() {
+      //TODO 本来はaxios呼び出し
+      //const index = this.users.findIndex((user) => user.userId === item.userId)
+      this.users.splice(this.deletedIndex, 1)
+      this.closeDelete()
+    },
+    deleteItem(item: any) {
+      this.deletedIndex = this.users.indexOf(item)
+      this.dialogDelete = true
+    },
+    closeDelete() {
+      this.dialogDelete = false
+      //DOMが更新されたら実行される。=ダイアログが閉じたら
+      this.$nextTick(() => {
+        this.deletedIndex = -1
+      })
     },
   },
 })
